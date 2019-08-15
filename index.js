f7683a3f2cde5112248b4b6e38657217445f8d49
EricssonLiu <EricssonLiu10@gmail.com>
2019-08-15 15:43:53 -0400

    Opps

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const SlackBot = require('slackbots');
 const axios = require('axios');
 
+const RoboMerlin_Username = 'UMDJG7V0V'
+
 const bot = new SlackBot({
-    token: 'xoxb-721230650737-727628267029-lv1mxKkFfKLqvVWpFHUoi9RO',
-    name: 'HDAvalon-bot'
+    token: 'xoxb-721230650737-727628267029-dQugSFaeQ4Py0RHAbbGbMVaQ',
+    name: 'RoboMerlin',
 });
 
 bot.on('start', () => {
@@ -16,4 +18,27 @@ bot.on('start', () => {
         'Avalon Bot Starting Up',
         params
     );
+
 });
+
+bot.on('message', (data) => {
+    console.log(data);
+    if (data.type !== 'message') {
+        return;
+    }
+    else if (!data.text.includes(RoboMerlin_Username)) {
+        return;
+    }
+    handleMessage(data.text);
+})
+
+function handleMessage(message) {
+
+    const params = {
+        icon_emoji: ':clown:'
+    };
+
+    if (message.includes('test')) {
+        bot.postMessageToChannel('slack-bot', 'Woof', params);
+    }
+}
