11868323b50edc4effa2e370234d3fcfeaa13329
Max Qi <max.qi@hubdoc.com>
2019-08-16 13:34:27 -0400

    fetch working

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 const SlackBot = require('slackbots');
 const axios = require('axios');
+const fetch = require('node-fetch');
 
 const RoboMerlin_Username = 'UMDJG7V0V'
 
 const bot = new SlackBot({
-    token: 'xoxb-721230650737-727628267029-A64v1vCjlnmchwtpMOWTVZM4',
+    token: 'xoxb-721230650737-727628267029-9TjwsMOVjfV8WNRDduU2Z1FH',
     name: 'RoboMerlin',
 });
 
 bot.on('start', () => {
-    const params = {
-        icon_emoji: ':clown:'
-    };
-
-    bot.postMessageToChannel(
-        'slack-bot',
-        'Avalon Bot Starting Up',
-        params
-    );
-
+    bot.postMessageToChannel('slack-bot', 'Avalon Bot Starting Up', {});
 });
 
 bot.on('message', (data) => {
@@ -35,13 +27,13 @@ bot.on('message', (data) => {
 let roles = {};
 let winningTeam;
 let merlinAssassination = null;
-function handleMessage(message) {
-    const params = {};
+
+async function handleMessage(message) {
     if (message.includes('newgame')) {
         roles = createGame ();
     }
     else if (message.includes('add')){
-        roles = addRoles (message, roles);
+        roles = await addRoles (message, roles);
     }
     else if (message.includes('win')){
         if (message.includes('good')){
@@ -59,7 +51,7 @@ function handleMessage(message) {
     }
     else if (message.includes('finish')){
         if (roles.count < 6){
-            return bot.postMessageToChannel('slack-bot', 'Not enough players have been entered (minimum 6)', params)
+            return bot.postMessageToChannel('slack-bot', 'Not enough players have been entered (minimum 6)', {})
         }
         const query = finishGame (roles, winningTeam);
     }
@@ -79,17 +71,41 @@ function createGame() {
     }
 };
 
-function addRoles(message, roles) {
+// function that takes in an id and returns the associated name
+async function getNameByID (id) {
+    let username;
+    if(!id) {
+        return;
+    }
+    else {
+        id = id
+            .replace('<@', '')
+            .replace('>', '');
+    }
+    await bot
+        .getUsers()
+        .then((users) => {
+            console.log(users.members);
+            for(const user of users.members) {
+                if (user.id == id) {
+                    username = user.name;
+                }
+            }
+        });
+    return username;
+}
+
+async function addRoles(message, roles) {
     if (!roles){
-        return bot.postMessageToChannel('slack-bot', 'Please start new game', params)
+        return bot.postMessageToChannel('slack-bot', 'Please start new game', {})
     }
     message = message.slice(17, message.length)
     const validRoles = ['merlin', 'percival', 'servant', 'mordred', 'morgana', 'assassin', 'oberon', 'minion']
     while (message.length !== 0){
         roles.count ++;
-        const currentPlayer = message.slice(0, message.indexOf(' '));
+        const currentPlayerID = message.slice(0, message.indexOf(' '));
         message = message.substr(message.indexOf(' ') + 1, message.length);
-        //const currentPlayer = something(currentPlayerID);
+        const currentPlayer = await getNameByID(currentPlayerID);
         const currentRole = message.slice(0, (message.indexOf(' ') === -1) ? message.length : message.indexOf(' ')).toLowerCase();
         if (message.indexOf(' ') === -1){
             message = '';
@@ -98,7 +114,7 @@ function addRoles(message, roles) {
             message = message.substr(message.indexOf(' ') + 1, message.length);
         }
         if (!(validRoles.includes(currentRole))){
-            return bot.postMessageToChannel('slack-bot', 'Role does not exist try again.', params)
+            return bot.postMessageToChannel('slack-bot', 'Role does not exist try again.', {})
         }
         if (currentRole === 'minion' || currentRole === 'servant'){
             roles[currentRole].push(currentPlayer);
@@ -110,6 +126,22 @@ function addRoles(message, roles) {
     console.log(roles);
 }
 
-function finishGame (roles, winningTeam, merlinAssassination){
-    
+async function finishGame (roles, winningTeam, merlinAssassination){
+    const gameData = {
+        roles: roles,
+        winningTeam: winningTeam,
+        merlinAssassination: merlinAssassination,
+    }
+    gameData = JSON.stringify(gamdData);
+    await fetch("http://10.254.59.72:3001/api/players", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: gameData,
+    }).then(function() {
+        return;
+    });
+    console.log("finished");
 }
